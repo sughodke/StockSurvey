2c25615105d7bccd1e88d08db68612cdf59a2bdd
Sid <sid.ghodke@gmail.com>
2016-09-26 22:37:40 -0700

    Loading S&P symbols in main

--- plot_stock_market.py
+++ plot_stock_market.py
@@ -76,6 +76,9 @@ from matplotlib.collections import LineCollection
 
 from sklearn import cluster, covariance, manifold
 
+from load_symbols import snp_500
+
+
 ###############################################################################
 # Retrieve the data from Internet
 
@@ -85,67 +88,7 @@ d1 = datetime.datetime(2003, 1, 1)
 d2 = datetime.datetime(2008, 1, 1)
 
 # kraft symbol has now changed from KFT to MDLZ in yahoo
-symbol_dict = {
-    'TOT': 'Total',
-    'XOM': 'Exxon',
-    'CVX': 'Chevron',
-    'COP': 'ConocoPhillips',
-    'VLO': 'Valero Energy',
-    'MSFT': 'Microsoft',
-    'IBM': 'IBM',
-    'TWX': 'Time Warner',
-    'CMCSA': 'Comcast',
-    'CVC': 'Cablevision',
-    'YHOO': 'Yahoo',
-    'DELL': 'Dell',
-    'HPQ': 'HP',
-    'AMZN': 'Amazon',
-    'TM': 'Toyota',
-    'CAJ': 'Canon',
-    'MTU': 'Mitsubishi',
-    'SNE': 'Sony',
-    'F': 'Ford',
-    'HMC': 'Honda',
-    'NAV': 'Navistar',
-    'NOC': 'Northrop Grumman',
-    'BA': 'Boeing',
-    'KO': 'Coca Cola',
-    'MMM': '3M',
-    'MCD': 'Mc Donalds',
-    'PEP': 'Pepsi',
-    'MDLZ': 'Kraft Foods',
-    'K': 'Kellogg',
-    'UN': 'Unilever',
-    'MAR': 'Marriott',
-    'PG': 'Procter Gamble',
-    'CL': 'Colgate-Palmolive',
-    'GE': 'General Electrics',
-    'WFC': 'Wells Fargo',
-    'JPM': 'JPMorgan Chase',
-    'AIG': 'AIG',
-    'AXP': 'American express',
-    'BAC': 'Bank of America',
-    'GS': 'Goldman Sachs',
-    'AAPL': 'Apple',
-    'SAP': 'SAP',
-    'CSCO': 'Cisco',
-    'TXN': 'Texas instruments',
-    'XRX': 'Xerox',
-    'LMT': 'Lookheed Martin',
-    'WMT': 'Wal-Mart',
-    'WBA': 'Walgreen',
-    'HD': 'Home Depot',
-    'GSK': 'GlaxoSmithKline',
-    'PFE': 'Pfizer',
-    'SNY': 'Sanofi-Aventis',
-    'NVS': 'Novartis',
-    'KMB': 'Kimberly-Clark',
-    'R': 'Ryder',
-    'GD': 'General Dynamics',
-    'RTN': 'Raytheon',
-    'CVS': 'CVS',
-    'CAT': 'Caterpillar',
-    'DD': 'DuPont de Nemours'}
+symbol_dict = snp_500(100)
 
 symbols, names = np.array(list(symbol_dict.items())).T
 
