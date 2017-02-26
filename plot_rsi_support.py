3aa775665149aa87a953b69545ef8fa4c98b6244
Sid <sid.ghodke@gmail.com>
2017-02-25 18:14:15 -0800

    Preparing data without arbitrary offsets Adding replay ability

--- plot_rsi_support.py
+++ plot_rsi_support.py
@@ -1,9 +1,12 @@
 import datetime
 
 import numpy as np
+import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import classification_report, accuracy_score
 
 from indicators import moving_average, relative_strength, bbands
 from load_ticker import load_data
@@ -11,88 +14,122 @@ from load_ticker import load_data
 fillcolor = "darkgoldenrod"
 
 
-def go(startdate=datetime.date(2016, 6, 1), enddate=datetime.date.today(), ticker='GLD', save=True):
+def go(startdate=datetime.date(2016, 6, 1), enddate=datetime.date.today(), ticker='GLD', replaydate=datetime.date.today()):
     r = load_data(startdate, enddate, ticker)
-    clf = train(r, ticker, save)
-    # replay(enddate, datetime.date.today(), ticker, clf)
+    X, y = prepare_data(r)
+    delta = enddate - replaydate
+
+    X_train, y_train = X[:-delta.days], y[:-delta.days]
+    X_replay, y_replay = X[-delta.days:], y[-delta.days:]
+
+    clf = train(X_train, y_train, ticker)
+    replay(clf, X_replay, y_replay, replaydate)
 
 
-# def replay(startdate=datetime.date(2016, 6, 1), enddate=datetime.date.today(), ticker='GLD', clf):
-#     r = load_data(startdate, enddate, ticker)
+def replay(clf, X, y, replaydate):
+    pred_y = clf.predict(X)
+    # prob_y = clf.predict_proba(X)
 
-def train(r, ticker, save=False):
+    for idx, truth, prediction in zip(range(len(X)), y, pred_y):
+        print('On {date} we expected {desc}; {met}'.format(
+            date=replaydate + datetime.timedelta(days=idx),
+            desc=describe_quadrant(prediction),
+            met='it did' if prediction == truth else 'instead ' + describe_quadrant(truth),
+        ))
+
+
+def rad_to_quadrant(a):
+    if a > 0:
+        if a < np.pi / 2:
+            return 0
+        if a < np.pi:
+            return 1
+    else:
+        if a > -np.pi:
+            return 2
+        if a > -np.pi / 2:
+            return 3
+    return -1
+
+
+def describe_quadrant(q):
+    return {
+        0: 'RSI to go up, moving toward B-Resistance',
+        1: '(unlikely) RSI to go down, moving toward B-Resistance',
+        2: 'RSI to go down, moving toward B-Support',
+        3: '(unlikely) RSI to go up, moving toward B-Support',
+        -1: 'Whoops'
+    }[q]
+
+
+def prepare_data(r):
     prices = r.adj_close
     ma14 = moving_average(prices, 14, type='simple')
     # ma20 = moving_average(prices, 20, type='simple')
 
     avgBB, upperBB, lowerBB = bbands(prices, 21, 2)
     pct_b = (prices - lowerBB) / (upperBB - lowerBB)
-    support = pct_b * 100  #
-    support = support[20:]  # remove empties
+    support = pct_b * 100.
 
     rsi = relative_strength(prices, 7)
     rsi_prime = np.gradient(rsi)
     # rsi_prime_zeros = np.diff(np.sign(rsi_prime))
 
-    rsi = rsi[20:]  # remove empties
-    # TODO Use gradient instead of diff?
-    d_rsi = np.diff(rsi)
-    d_support = np.diff(support)
-    # d_rsi = np.gradient(rsi)
-    # d_support = np.gradient(support)
+    d_rsi = np.gradient(rsi)
+    d_support = np.gradient(support)
     arctan = np.arctan2(d_support, d_rsi)
 
     X = np.vstack([
-        avgBB[-len(rsi):],
-        rsi_prime[-len(rsi):],
-        ma14[-len(rsi):],
-        (prices - ma14)[-len(rsi):],
+        avgBB,
+        rsi_prime,
+        ma14,
+        (prices - ma14),
         support,
         rsi,
-        relative_strength(prices, 14)[-len(rsi):],
-        relative_strength(prices, 21)[-len(rsi):],
+        relative_strength(prices, 14),
+        relative_strength(prices, 21)
     ]).T
 
+    X = pd.DataFrame(X)
+    X = X.dropna()
+
+    arctan = arctan[-len(X):]
     y = np.flipud(moving_average(np.flipud(arctan), 2, 'exponential'))
+    y = np.array(map(rad_to_quadrant, y))
 
-    # average of RSI, Support not diff
-    # y = np.flipud(moving_average(np.flipud(np.roll(rsi, -1)), 4, 'exponential'))
+    print('X: %r  y: %r' % (X.shape, y.shape))
 
-    def rad_to_quadrant(a):
-        if a > 0:
-            if a < np.pi / 2:
-                return 0
-            if a < np.pi:
-                return 1
-        else:
-            if a > -np.pi:
-                return 2
-            if a > -np.pi / 2:
-                return 3
-        return -1
-    y = np.array([-1] + map(rad_to_quadrant, y))
-    # y = np.array(map(rad_to_quadrant, y))
+    return X, y
+
+
+def train(X, y, ticker):
+    decision_tree = True
 
     print('{} has {} samples with {} features'.format(ticker, *X.shape))
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-
-    clf = DecisionTreeClassifier(random_state=0)
+    if decision_tree:
+        clf = DecisionTreeClassifier(random_state=0)
+    else:
+        clf = SGDClassifier(loss="hinge", penalty="l2")
     clf.fit(X_train, y_train)
 
-    print('Our classifier has scored with %.3f accuracy' % clf.score(X_test, y_test))
+    y_pred = clf.predict(X_test)
+    print('Our classifier has scored with %.3f accuracy' % accuracy_score(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
 
-    feat = dict(zip(['avgBB', 'rsi_prime', 'ma14', 'prices - ma14', 'bol_pct', 'rsi_7', 'rsi_14', 'rsi_21'],
-                    clf.feature_importances_))
+    if decision_tree:
+        feat = dict(zip(['avgBB', 'rsi_prime', 'ma14', 'prices - ma14', 'bol_pct', 'rsi_7', 'rsi_14', 'rsi_21'],
+                        clf.feature_importances_))
 
-    print('With the following contributors')
-    for k in sorted(feat, key=feat.get, reverse=True):
-        v = feat[k]
-        print('%.2f %s' % (v, k))
+        print('With the following contributors')
+        for k in sorted(feat, key=feat.get, reverse=True):
+            v = feat[k]
+            print('%.2f %s' % (v, k))
 
     return clf
 
 
 if __name__ == '__main__':
-    go(ticker='NVDA', enddate=datetime.date(2017, 2, 21), save=False)
+    go(ticker='NVDA', replaydate=datetime.date(2017, 1, 21))
 
