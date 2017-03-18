3248a3b711a9641a13e2c3db209d8cd9f92dbaac
Sid <sid.ghodke@gmail.com>
2017-03-18 10:28:11 -0700

    Adding Security model that can save, sync and process computed indicators

---
+++ security_model.py
@@ -0,0 +1,86 @@
+import datetime
+import os
+import joblib
+import numpy as np
+
+from load_ticker import load_data
+
+# Legacy functions to preserve recarray when merging
+# from numpy.lib.recfunctions import merge_arrays, stack_arrays
+# stack_arrays((self.daily, delta), asrecarray=True, flatten=True)
+
+
+cwd = os.path.dirname(__file__)
+ds_path = './DataStore/'
+
+
+class Security(object):
+    STARTDATE = datetime.date(2016, 6, 1)
+    store_dir = os.path.join(cwd, ds_path)
+
+    def __init__(self, ticker='GLD'):
+        self.ticker = ticker
+        self.startdate = self.STARTDATE
+        self.enddate = None
+        self.daily = None
+
+        self.sync()
+
+    def sync(self):
+        today = self._today
+        if not self.enddate:
+            self.enddate = self.STARTDATE
+
+        if self.enddate < today:
+            delta = load_data(self.enddate + datetime.timedelta(days=1), today, self.ticker)
+            delta = self._fix_dtypes(delta)
+
+            if self.daily is not None:
+                self.daily = np.hstack((self.daily, delta)).view(np.recarray)
+            else:
+                self.daily = delta
+
+            self.daily = self._fix_dtypes(self.daily)
+            self.enddate = today
+
+    @staticmethod
+    def _fix_dtypes(recarr):
+        """
+        MLAB is a POS
+        Overwriting the datatypes to ensure serialize works smoothly
+        """
+        return recarr.astype([('date', '<M8[D]'), ('open', '<f8'),
+                              ('high', '<f8'), ('low', '<f8'),
+                              ('close', '<f8'), ('volume', '<i8'),
+                              ('adj_close', '<f8')])
+
+    @classmethod
+    def _filename(cls, ticker):
+        return os.path.join(cls.store_dir, '{}'.format(ticker))
+
+    @property
+    def _today(self):
+        return datetime.date.today()
+
+    def save(self):
+        joblib.dump(self, self._filename(self.ticker), compress=False)
+
+    @classmethod
+    def load(cls, ticker, sync=False):
+        try:
+            security = joblib.load(cls._filename(ticker))
+            if sync:
+                security.sync()
+            return security
+        except IOError as e:
+            return Security(ticker)
+
+
+class RSIMixin(object):
+    def computed_indicators(self):
+        prices = r.adj_close
+        rsi = relative_strength(prices, 7)
+        rsi_prime = np.gradient(rsi)
+        rsi_prime_zeros = np.where(np.diff(np.sign(rsi_prime)))[0]
+        rsi_ma10 = moving_average(rsi, 10, type='exponential')
+        rsi_ma_cross = np.where(np.diff(np.sign(rsi - rsi_ma10)))[0]
---
+++ security_test.py
@@ -0,0 +1,49 @@
+import os
+import unittest
+from security_model import *
+
+
+class SecurityTestHarness(Security):
+    _today = datetime.date(2016, 12, 1)
+    store_dir = '/tmp/'
+
+
+class TestSyncMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.security = SecurityTestHarness()
+
+    def test_enddate(self):
+        self.assertEqual(self.security.enddate,
+                         SecurityTestHarness._today)
+        self.assertEqual(self.security.daily.date[-1],
+                         SecurityTestHarness._today)
+
+    def test_nextday(self):
+        self.security._today = datetime.date(2016, 12, 2)
+        self.security.sync()
+        self.assertEqual(self.security.daily.date[-1],
+                         self.security._today)
+
+
+class TestStoreMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.security = SecurityTestHarness()
+
+    def test_save(self):
+        self.security.save()
+        self.assertTrue(os.path.isfile(self.security._filename(self.security.ticker)),
+                        True)
+
+    def test_load(self):
+        self.security.save()
+        ticker = self.security.ticker
+        loaded_security = SecurityTestHarness.load(ticker)
+        self.assertEqual(self.security.daily.shape,
+                         loaded_security.daily.shape)
+        # loaded_security._today = datetime.date(2016, 12, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
