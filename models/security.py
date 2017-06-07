e980e76f055582214aebf56bfb3ec2d3b1856b2a
Sid <sid.ghodke@gmail.com>
2017-06-06 22:34:31 -0700

    Refactor Span class

--- models/security.py
+++ models/security.py
@@ -1,15 +1,13 @@
 import datetime
+import logging
 import os
+
 import joblib
 import pandas as pd
 
-from models.plotter import PlotMixin
-from models.indicators import RSIMixin, TheEvaluator
-from models.directors import TheDecider, NumpyDecider
+from models.span import Span
 from models.timespan import AddTimeSpan
 from util import load_data, cwd
-
-import logging
 
 ds_path = 'DataStore'
 store_dir = os.path.join(cwd, ds_path)
@@ -74,22 +72,3 @@ class Security(AddTimeSpan):
     def span(self, span):
         """acts as a context manager"""
         return Span(self, span)
-
-
-class Span(object):
-    def __init__(self, security, span=None):
-        # TODO: Weekly Span should increase the look back to 1.5 years
-        self.dataset = getattr(security, span, security.daily)
-        self.events = []
-
-        self.ticker = security.ticker
-        self.span = span or 'daily'
-
-        self.calc = RSIMixin(self.dataset)
-        self.decide = NumpyDecider(self.dataset, self.calc)
-        self.eval = TheEvaluator(self.dataset)
-        self.plot = PlotMixin(self.dataset, self.ticker,
-                              self.calc, self.decide, self.eval)
-
-    def recent_events(self, last_n):
-        return [str(event) for event in self.events][:last_n]
